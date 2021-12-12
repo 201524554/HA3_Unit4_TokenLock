dc06f86871381aed83d0e8af08434c2a7fdce8eb
dd <43158434+201524554@users.noreply.github.com>
2021-12-13 04:03:25 +0900

    Create ERC-20.sol

---
+++ ERC-20.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.10;
+
+interface ERC20Interface {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function transferFrom(address spender, address recipient, uint256 amount) external returns (bool);
+    
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Transfer(address indexed spender, address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 oldAmount, uint256 amount);
+}
+
+library SafeMath {
+  	function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+		uint256 c = a * b;
+		assert(a == 0 || c / a == b);
+		return c;
+  	}
+
+  	function div(uint256 a, uint256 b) internal pure returns (uint256) {
+	    uint256 c = a / b;
+		return c;
+  	}
+
+  	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+		assert(b <= a);
+		return a - b;
+  	}
+
+  	function add(uint256 a, uint256 b) internal pure returns (uint256) {
+		uint256 c = a + b;
+		assert(c >= a);
+		return c;
+	}
+}
+
+abstract contract OwnerHelper {
+
+    address[3] internal owners;
+
+  	event OwnershipTransferred(address indexed preOwner, address indexed nextOwner);
+
+  	modifier onlyOwner {
+		require(msg.sender == owners[0] || msg.sender == owners[1] || msg.sender == owners[2], "OwnerHelper: caller is not owner");
+		_;
+  	}
+
+  	constructor() {
+            owners[0] = msg.sender;
+  	}
+
+    function owner() public view virtual returns (address[3] memory) {
+           return owners;
+    }
+}
+
